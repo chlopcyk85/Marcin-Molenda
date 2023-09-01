496b95ded8721509dfccebdfe05f04cf83b69387
Marcin <chlopcyk85@gmail.com>
2023-09-01 12:15:42 +0200

    dodanie zmiennych uzytkownika

---
+++ KredytFutureCollar.py
@@ -0,0 +1,6 @@
+#wartosci podane przez użytkownia(terminal)
+oprocentowanie_kredytu=float(input("Podaj jakie ma być stałe oprocentowanie Twojego kredytu: "))
+stala_rata = int(input("Podaj wysokosc stałej raty kredytu: "))
+wysokosc_kredytu=int(input("Podaj wartość początkową kredytu, kwota pożyczki: "))
+kredyt_na10 = wysokosc_kredytu/10
+
