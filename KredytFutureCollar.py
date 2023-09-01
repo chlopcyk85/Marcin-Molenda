e828a6513fff02eebdf4bd874a5cebcfa5d70f98
Marcin <chlopcyk85@gmail.com>
2023-09-01 12:17:10 +0200

    wartosci inflacji

--- KredytFutureCollar.py
+++ KredytFutureCollar.py
@@ -4,3 +4,29 @@ stala_rata = int(input("Podaj wysokosc stałej raty kredytu: "))
 wysokosc_kredytu=int(input("Podaj wartość początkową kredytu, kwota pożyczki: "))
 kredyt_na10 = wysokosc_kredytu/10
 
+#wysokosc inflacji na przestrzeni 24 miesiecy
+styczen_1 = 1.592824484
+luty_1 = -0.453509101
+marzec_1 = 2.324671717
+kwiecien_1 = 1.261254407
+maj_1 = 1.782526286
+czerwiec_1 = 2.329384541
+lipiec_1 = 1.502229842
+sierpien_1 = 1.782526286
+wrzesien_1 = 2.328848994
+pazdziernik_1 = 0.616921348
+listopad_1 = 2.352295886
+grudzien_1 = 0.337779545
+styczen_2 = 1.577035247
+luty_2 = -0.292781443
+marzec_2 = 2.48619659
+kwiecien_2 = 0.267110318
+maj_2 = 1.417952672
+czerwiec_2 = 1.054243267
+lipiec_2 = 1.480520104
+sierpien_2 = 1.577035247
+wrzesien_2 = -0.07742069
+pazdziernik_2 = 1.165733399
+listopad_2 = -0.404186718
+grudzien_2 = 1.499708521
+
